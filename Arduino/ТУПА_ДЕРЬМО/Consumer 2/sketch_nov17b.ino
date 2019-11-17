ea70ab5add759321dfbc45c42c37af1b412a08ea
Vait-Marsovich <55848496+Vait-Marsovich@users.noreply.github.com>
2019-11-17 21:16:13 +0300

    Из говна можно вылепить только говно

---
+++ Arduino/ТУПА_ДЕРЬМО/Consumer 2/sketch_nov17b.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+
+uint8_t _speed = 0;
+uint8_t light = 0;
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+
+  Wire.begin(0x11); 
+  Wire.onReceive(receiveEvent);
+}
+
+void loop() {
+  analogWrite(3, _speed);
+  analogWrite(5, light);
+}
+
+void receiveEvent(int bytes) {
+  if(bytes>1){
+    _speed = Wire.read();
+    light = Wire.read();
+  }
+}
+
