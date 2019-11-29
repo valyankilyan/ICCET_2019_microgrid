a444727ac8aed04973dcda12c1b2fe0f58030e59
valyankilyan <valentin.kil.2002@gmail.com>
2019-11-29 15:58:57 +0300

    add working scene

--- Arduino/consumer/Scene/Scene.ino
+++ Arduino/consumer/Scene/Scene.ino
@@ -1,49 +1,41 @@
-#include <Wire.h>
 #include <SD.h>                      // need to include the SD library
 //#define SD_ChipSelectPin 53  //example uses hardware SS pin 53 on Mega2560
-#define SD_ChipSelectPin 4  //using digital pin 4 on arduino nano 328, can use other pins
+#define SD_ChipSelectPin 10  //using digital pin 4 on arduino nano 328, can use other pins
 #include <TMRpcm.h>           //  also need to include this library...
 #include <SPI.h>
+#include <Wire.h>
 
-TMRpcm tmrpcm;
+TMRpcm tmrpcm;   // create an object for use in this sketch
 
-uint8_t music = 0;
-uint8_t sound = 0;
-
-unsigned long time = 0;
+bool work = 1;
+int sound_id = 1;
 
 void setup(){
-  tmrpcm.speakerPin = 9; 
-  
-  Wire.begin(0x12); 
+  Wire.begin(0x10);
   Wire.onReceive(receiveEvent);
+
+  tmrpcm.speakerPin = 9; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
+
+  tmrpcm.play("3.wav"); //the sound file "music" will play each time the arduino powers up, or is reset
 }
 
-void loop() {
-  
-  if(tmrpcm.isPlaying() && millis() - time > 50 ) {      
-        time = millis();    
-  }else
-  if(millis() - time > 500){     
-      time = millis(); 
-  }
 
-  switch(music){
-    case 1: tmrpcm.play("1.wav"); break;
-    case 2: tmrpcm.play("2.wav"); break;
-    default: break;
-  }
 
-  if(sound == 1){
-    tmrpcm.volume(1);
-  }else{
-    tmrpcm.volume(0);
+void loop(){  
+  if(sound_id == 1){ //send the letter p over the serial monitor to start playback
+    tmrpcm.play("1.wav");
+  }    
+  if(sound_id == 2){ //send the letter p over the serial monitor to start playback
+    tmrpcm.play("2.wav");
+  }
+  if(sound_id == 3){ //send the letter p over the serial monitor to start playback
+    tmrpcm.play("3.wav");
   }
 }
 
 void receiveEvent(int bytes) {
-  if(bytes>1){
-    music = Wire.read();
-    sound = Wire.read();
+  if(bytes > 1){
+    work = Wire.read();
+    sound_id = Wire.read();
   }
 }
--- Rpi/main.py
+++ Rpi/main.py
@@ -3,11 +3,21 @@ from colorama import Fore
 import time
 import os
 
-ADDR_1 = 16
-ADDR_2 = 17
-ADDR_3 = 18
-ADDR_4 = 19
-ADDR_5 = 20
+ADDR_scene = 16
+scene_data = [1, 100] #sound id, sound level
+music_list = ["Soccer physics soundtrack", "Sergey's trap", "Shrek theme"]
+
+ADDR_light = 17
+light_data = [100, 100, 100, 100]#bright, red, green, blue
+
+ADDR_radio = 18
+radio_data = [1]#on or off
+
+ADDR_vegetables = 19
+vegetables_data = [30, 100, 100, 100, 100]#temperature, bright, red, green, blue
+
+ADDR_wheel = 20
+wheel_data = [100, 100]#speed, bright
 
 
 def writeData(addr, data):
@@ -16,25 +26,60 @@ def writeData(addr, data):
    		bus.i2c_rdwr(msg)	
 
 
-def writeData_1():
+def change_scene_music_id():
+	print("Choise track")
+	it = 1;
+	for i in music_list:
+		print it, "-", i
+		it+= 1
+	music_id =  input("Enter your choise: ")
+	os.system('clear')
+	if music_id > 3:
+		print(Fore.RED + "##ERROR" + Fore.WHITE + " No, sorry, we haven't")
+	else:
+		return music_id
+
+def writeData_scene():
+	print("Choise what do you want")
+	print("1 - Change music")
+	print("2 - Change music level")
+	print("0 - exit")
+	what = input("Enter your choise: ")
+	os.system('clear')
+
+	music_id = scene_data[0]
+	soumd_level = scene_data[1]
+
+	if what == 0:
+		return
+	if what == 1:
+		music_id = change_scene_music_id()
+	if what == 2:
+		return
+	if what > 2:
+		print(Fore.RED + "##ERROR" + Fore.WHITE + " No, sorry, we haven't")
+	else:
+		scene_data[0] = music_id
+		writeData(ADDR_scene, scene_data)
+
+
+
+def writeData_light():
 	data = []
 
-def writeData_2():
+def writeData_radio():
 	data = []
 
-def writeData_3():
+def writeData_vegetables():
 	data = []
 
-def writeData_4():
-	data = []
-
-def writeData_5():
+def writeData_wheel():
 	data = []
 
 
 def prosumer_choise():
 	print("Choise what do you want")
-	print("1 - ")
+	print("1 - Scene")
 	print("2 - ")
 	print("3 - ")
 	print("4 - ")
@@ -45,15 +90,15 @@ def prosumer_choise():
 	if what == 0:
 		return
 	if what == 1:
-		writeData_1()
+		writeData_scene()
 	if what == 2:
-		writeData_2()
+		writeData_light()
 	if what == 3:
-		writeData_3()
+		writeData_radio()
 	if what == 4:
-		writeData_4()
+		writeData_vegetables()
 	if what == 5:
-		writeData_5()
+		writeData_wheel()
 	if what > 5:
 		print(Fore.RED + "##ERROR" + Fore.WHITE + " No, sorry, we haven't")
 		
