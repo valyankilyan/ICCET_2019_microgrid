98ba5ecd12b74dc99f48aa6a375332f29174094d
valyankilyan <valentin.kil.2002@gmail.com>
2019-12-11 20:22:40 +0300

    test govono

--- Arduino/consumer/Vegetable_house/Vegetable_house.ino
+++ Arduino/consumer/Vegetable_house/Vegetable_house.ino
@@ -1,5 +1,5 @@
+#include <SD.h>                      // need to include the SD library
 #include <Wire.h>
-#include <SPI.h>
 #include <TroykaThermometer.h>
 
 // red pin 5, green pin 6, blue pin 9
@@ -20,7 +20,7 @@ void setup() {
     pinMode(light_pin[i], OUTPUT);
 
   Wire.begin(0x12); 
-  Wire.onReceive(receiveEvent);
+  Wire.onReceive(receiveData);
   Wire.onRequest(requestEvent);
 }
 
@@ -29,7 +29,7 @@ void loop() {
   sensor = thermometer.getTemperatureC();
   temp = min(max(0, (temperature - sensor)*100), 255);
   
-  analogWrite(HOT_PIN, temp);
+  analogWrite(HOT_PIN, 255);
   
   for(int i = 0; i < 3; i++)
     analogWrite(light_pin[i], light[i]);
@@ -38,9 +38,10 @@ void loop() {
      tim = millis();
      Serial.println("hello");
   }
+  delay(50);
 }
 
-void receiveEvent(int bytes) {
+void receiveEvent(int bytes) {  
   if(bytes > 1){
     temperature = Wire.read();
     bright = double(Wire.read()) / 100;
@@ -56,6 +57,87 @@ void receiveEvent(int bytes) {
   }
 }
 
+
+void receiveData(int byteCount){
+
+  while(Wire.available()) {
+    temperature = Wire.read();
+    bright = double(Wire.read()) / 100;
+    for(int i = 0; i < 3; i++)
+      light[i] = double(Wire.read() * bright) / 100 * 255;
+    Wire.read();
+
+    Serial.println("start reading");
+    Serial.println(temperature);
+    Serial.println(bright);
+    for(int i = 0; i < 3; i++)
+      Serial.println(light[i]);
+    Serial.println("end reading");
+    /*number = Wire.read();
+    Serial.print("data received: ");
+    Serial.println(number);
+
+    if (number == 1){
+      if (state == 0){
+        digitalWrite(13, HIGH); // set the LED on
+        state = 1;
+      }
+      else{
+        digitalWrite(13, LOW); // set the LED off
+        state = 0;
+      }
+    }*/
+  }
+}
+
 void requestEvent(){
   Wire.write((uint8_t *)&sensor, sizeof(&sensor));
 }
+/*
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x12
+int number = 0;
+int state = 0;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  Serial.begin(9600); // start serial for output
+  
+  Wire.begin(SLAVE_ADDRESS);
+
+ 
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+  Serial.println("Ready!");
+}
+
+void loop() {
+  delay(100);
+}
+
+void receiveData(int byteCount){
+
+  while(Wire.available()) {
+    number = Wire.read();
+    Serial.print("data received: ");
+    Serial.println(number);
+
+    if (number == 1){
+      if (state == 0){
+        digitalWrite(13, HIGH); // set the LED on
+        state = 1;
+      }
+      else{
+        digitalWrite(13, LOW); // set the LED off
+        state = 0;
+      }
+    }
+  }
+}
+
+  
+void sendData(){
+  Wire.write(number);
+}*/
---
+++ Rpi/tests/data.py
@@ -0,0 +1,67 @@
+from smbus2 import SMBus, i2c_msg
+import time
+import os
+import sys
+
+
+def __init__():
+	print("init data")
+
+def write(addr, data):
+	try:
+		with SMBus(1) as bus:
+			try:
+				msg = i2c_msg.write(addr, data)
+				bus.i2c_rdwr(msg)	
+			
+			except IOError as e:
+				print "I/O error({0}): {1}".format(e.errno, e.strerror)
+				time.sleep(1)
+			
+			except ValueError:
+				print "Could not convert data to an integer."
+				time.sleep(1)
+			
+			except:
+				print "Unexpected error:", sys.exc_info()[0]
+				time.sleep(1)
+				raise
+	except: 
+		print "u have not i2c"
+
+def read(addr):
+	print "read"
+	try:
+		with SMBus(1) as bus:
+			try:
+			    write = i2c_msg.write(self.addr, [200])
+			    read = i2c_msg.read(self.addr, 2)				
+			    self.bus.i2c_rdwr(write, read)
+			    for i in read:
+			    	print(read[i])				 
+
+			except IOError as e:
+				print "I/O error({0}): {1}".format(e.errno, e.strerror)
+				time.sleep(1)
+			
+			except ValueError:
+				print "Could not convert data to an integer."
+				time.sleep(1)
+			
+			except:
+				print "Unexpected error:", sys.exc_info()[0]
+				time.sleep(1)
+				raise
+	except: 
+		print "u have not i2c"
+
+while(True):
+	print "1 - write"
+	print "2 - read"
+	try:
+		what = input()
+	except:
+		print "suck a cock"
+
+	if what == 2:
+		read(18)
