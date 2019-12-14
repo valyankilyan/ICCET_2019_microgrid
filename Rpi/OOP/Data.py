ce915a6531a63795c8f37e34c35a20b377bc036d
valyankilyan <valentin.kil.2002@gmail.com>
2019-12-14 11:32:54 +0300

    add docker compose file

--- Rpi/OOP/Data.py
+++ Rpi/OOP/Data.py
@@ -33,5 +33,26 @@ class Data:
 		except: 
 			print "u have not i2c"
 
-	def read(self):
-		print "read"
+	def read(self, addr, data):
+		print "read_byte"
+		try:
+			with SMBus(1) as bus:
+				try:
+					msg = i2c_msg.read(addr, len(data))
+					read = bus.i2c_rdwr(msg)
+					for i in msg:
+						print int(i)
+				except IOError as e:
+					print "I/O error({0}): {1}".format(e.errno, e.strerror)
+					time.sleep(1)
+				
+				except ValueError:
+					print "Could not convert data to an integer."
+					time.sleep(1)
+				
+				except:
+					print "Unexpected error:", sys.exc_info()[0]
+					time.sleep(1)
+					raise
+		except: 
+			print "u have not i2c"
