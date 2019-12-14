f97fcb66b673740156bf2a11bd282943e14c41bf
valyankilyan <valentin.kil.2002@gmail.com>
2019-12-14 23:37:41 +0300

    add not latest file system

---
+++ Rpi/OOP_with_WebSocketServer/SocketServer.py
@@ -0,0 +1,34 @@
+import threading
+from Server_files.SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import random, time
+
+
+class SocketServer(WebSocket):
+
+	def handleMessage(self):
+		# echo message back to client
+		self.sendMessage(self.data)
+		print(self.data)
+
+		def run():
+			self.send("poshel naher kozel")
+			for i in range(3):
+				time.sleep(1)
+				self.send(str(i*135))
+				time.sleep(1)
+			print("thread terminating...")
+			if self.data == 'scene':
+				threading.Thread(target=run).start()
+
+	def handleConnected(self):
+		print(self.address, 'connected')
+
+	def handleClose(self):
+		print(self.address, 'closed')
+
+	def send(self, string):
+		self.sendMessage(string)
+
+
+server = SimpleWebSocketServer('', 8001, SimpleEcho)
+server.serveforever()
