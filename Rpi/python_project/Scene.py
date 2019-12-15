ed78f1464f1dd7dfbee7ea67d31b017e10b519d8
valyankilyan <valentin.kil.2002@gmail.com>
2019-12-15 22:10:30 +0300

    up

--- Rpi/python_project/Scene.py
+++ Rpi/python_project/Scene.py
@@ -20,9 +20,10 @@ class Scene:
 			var.scene_data[3] = 1 if dic["retryTrack"] == True else 0 
 		except:
 			error.log("Scene rewrite data")
-		print var.scene_data[1:]
+		data = var.scene_data[1:]
+		print data
 
-		i2c.write(var.scene_addr, var.scene_data[1:])
+		i2c.write(var.scene_addr, data)
 	
 
 {u'retryTrack': False, u'bassboosted': False, u'track': 3, u'type': u'Scene', u'currentSupply': True, u'tariff': False}
