a3ec349d17cfa9337cca74f47f7f4c93b5ccf621
valyankilyan <valentin.kil.2002@gmail.com>
2019-12-17 15:09:18 +0300

    fix

--- Rpi/python_project/main.py
+++ Rpi/python_project/main.py
@@ -22,10 +22,10 @@ class Server(WebSocket):
             error.log("data sent isn't JSON")
             self.sendMessage("you sent not JSON")
 
-        # try:
-        consumer.new_data(dic)
-        # except: 
-        #     error.log("consumer doesn't work")
+        try:
+            consumer.new_data(dic)
+        except: 
+            error.log("consumer doesn't work")
     
 
     def handleConnected(self):
@@ -36,6 +36,10 @@ class Server(WebSocket):
 
     def send(self, string):
         self.sendMessage(string)
+
+    def run():
+        self.send(raw_input())
+    threading.Thread(target = run).start()
 
 
 print "starting server"
